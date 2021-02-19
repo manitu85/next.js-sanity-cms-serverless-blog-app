f52b775ec60e757810c1c22decc96a1ed52ed87a
manitu85 <manitu.psy@gmail.com>
2021-02-19 20:16:23 +0100

    #

--- pages/index.js
+++ pages/index.js
@@ -17,16 +17,6 @@ export default function Home() {
             <a href="https://nextjs.org">Next.js + Sanity CMS Blog App</a>
           </h1>
         </main>
-        <footer className={styles.footer}>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Powered by{' '}
-            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-          </a>
-        </footer>
       </Container>
     </div>
   );
