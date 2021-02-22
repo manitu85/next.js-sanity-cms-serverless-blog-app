3d454718a9b0b14426aa6fce7b2addb797fba4c9
manitu85 <manitu.psy@gmail.com>
2021-02-22 23:52:28 +0100

    add sanity client and fetch data from sanity api

--- pages/index.js
+++ pages/index.js
@@ -2,19 +2,37 @@ import { Row, Col } from 'react-bootstrap';
 import AuthorIntro from '@/components/AuthorIntro';
 import CardListItem from '@/components/CardListItem';
 import CardItem from '@/components/CardItem';
+import { getAllBlogs } from '@/lib/api';
 
-const Home = () => {
+export async function getStaticProps(context) {
+  console.log('Calling getStaticProps');
+  const blogs = await getAllBlogs();
+  return {
+    props: {
+      blogs
+    }
+  };
+}
+
+const Home = ({ blogs }) => {
+  console.log('blogs :>> ', blogs);
   return (
     <div className="blog-detail-page">
       <AuthorIntro />
+      {JSON.stringify(blogs)}
       <hr />
       <Row className="mb-5">
         <Col md="10">
           <CardListItem />
         </Col>
-        <Col md="4">
+        {blogs.map((blog) => (
+          <Col key={blog.slug} md="4">
+            <CardItem title={blog.title} subtitle={blog.subtitle} />
+          </Col>
+        ))}
+        {/* <Col md="4">
           <CardItem />
-        </Col>
+        </Col> */}
       </Row>
     </div>
   );
