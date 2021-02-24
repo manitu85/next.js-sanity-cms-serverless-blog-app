bb833883c67bcbc648886d3b30d53a0056fa64c7
manitu85 <manitu.psy@gmail.com>
2021-02-24 23:44:44 +0100

    done with single blog page

--- pages/blogs/[slug].js
+++ pages/blogs/[slug].js
@@ -1,10 +1,10 @@
+import { Row, Col } from 'react-bootstrap';
+import BlogHeader from '@/components/BlogHeader';
 import { getAllBlogs, getBlogBySlug } from '@/lib/api';
 
 export async function getStaticPaths() {
   const blogs = await getAllBlogs();
-
   const paths = blogs?.map((blog) => ({ params: { slug: blog.slug } }));
-
   return {
     paths,
     fallback: false
@@ -12,8 +12,6 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  console.log('Fetching blog by', params.slug);
-
   const blog = await getBlogBySlug(params.slug);
   return {
     props: { blog }
@@ -22,7 +20,32 @@ export async function getStaticProps({ params }) {
 
 const BlogDetail = ({ blog }) => {
   console.log('Displaying page');
-  return <h1>Hello Detail Page - {blog?.slug}</h1>;
+  return (
+    <>
+      <Row>
+        <Col md={{ span: 10, offset: 1 }}>
+          <BlogHeader
+            title={blog.title}
+            subtitle={blog.subtitle}
+            coverImage={blog.coverImage}
+            author={blog.author}
+            date={blog.date}
+          />
+          <hr />
+          {/* Blog Content Here */}
+          It is a long established fact that a reader will be distracted by the
+          readable content of a page when looking at its layout. The point of
+          using Lorem Ipsum is that it has a more-or-less normal distribution of
+          letters, as opposed to using 'Content here, content here', making it
+          look like readable English. Many desktop publishing packages and web
+          page editors now use Lorem Ipsum as their default model text, and a
+          search for 'lorem ipsum' will uncover many web sites still in their
+          infancy. Various versions have evolved over the years, sometimes by
+          accident, sometimes on purpose (injected humour and the like).
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default BlogDetail;
