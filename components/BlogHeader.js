a33485f86e1581c5291898d3edb3a62ca63aecb9
manitu85 <manitu.psy@gmail.com>
2021-02-25 00:47:39 +0100

    add blog header

--- components/BlogHeader.js
+++ components/BlogHeader.js
@@ -1,7 +1,7 @@
 const BlogHeader = ({ title, subtitle, coverImage, date, author }) => {
   return (
     <div className="blog-detail-header">
-      <p className="lead mb-0">
+      <p className="lead mb-3">
         <img
           src={author?.avatar}
           className="rounded-circle mr-3"
@@ -14,7 +14,7 @@ const BlogHeader = ({ title, subtitle, coverImage, date, author }) => {
       <h1 className="font-weight-bold blog-detail-header-title mb-0">
         {title}
       </h1>
-      <h2 className="blog-detail-header-subtitle mb-3">{subtitle}</h2>
+      <h3 className="blog-detail-header-subtitle mb-3">{subtitle}</h3>
       {/* Check if contains cover image */}
       <img
         className="img-fluid rounded"
